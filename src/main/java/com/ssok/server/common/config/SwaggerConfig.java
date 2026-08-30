9684a90690b2c65bc88eed88d4de3513060ede89
binsm24 <binsm24@sookmyung.ac.kr>
2026-08-30 15:09:26 +0900

    Feat: 팀원 삭제 OWNER 권한 검증 구현

--- src/main/java/com/ssok/server/common/config/SwaggerConfig.java
+++ src/main/java/com/ssok/server/common/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.ssok.server.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,18 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Ssok Server API")
                         .description("Ssok Server API 명세서")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("bearerAuth")
+                );
     }
 }
